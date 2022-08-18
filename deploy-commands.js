d809ba3b43bc474bef750fa68e5f3dda8d267b05
PaperClip <drumblebotxromir@gmail.com>
2022-08-18 07:24:39 +0000

    Update deploy-commands.js and index.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,18 +1,22 @@
-const {REST} = require('@discordjs/rest');
-const fs = require('fs');
-const path = require('path');
-const {discordToken, clientId} =  require('./config.json');
-const {Routes} = require('discord-api-types/v9');
-console.log('Credentials loaded and commands are now being pushed to the discord servers!');
+const { REST } = require("@discordjs/rest");
+const fs = require("fs");
+const path = require("path");
+const { discordToken, clientId } = require("./config.json");
+const { Routes } = require("discord-api-types/v9");
+console.log(
+  "Credentials loaded and commands are now being pushed to the discord servers!"
+);
 exports.initializeCommands = async () => {
-    const commands = [];
-    const commandsFiles = fs.readdirSync(path.resolve(__dirname, './commands')).filter(x => x.endsWith('.js'));
+  const commands = [];
+  const commandsFiles = fs
+    .readdirSync(path.resolve(__dirname, "./commands"))
+    .filter((x) => x.endsWith(".js"));
 
-    for(const file of commandsFiles){
-        const command = require(`./commands/${file}`)
-        commands.push(command.data.toJSON());
-    }
+  for (const file of commandsFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+  }
 
-    const rest = new REST({version: '9'}).setToken(discordToken);
-    await rest.put(Routes.applicationCommands(clientId), {body: commands});
+  const rest = new REST({ version: "9" }).setToken(discordToken);
+  await rest.put(Routes.applicationCommands(clientId), { body: commands });
 };
